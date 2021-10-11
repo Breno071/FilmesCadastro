8054392dec19f0b3bdbe66ded60567b5cf8f625e
Breno071 <brenonarde@gmail.com>
2021-10-11 15:41:35 -0300

    Adiciona arquivos

---
+++ Models/ContextFilmes/FilmesContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+
+namespace FilmesCadastro.Models.ContextFilmes
+{
+    public class FilmesContext : DbContext
+    {
+        
+        public DbSet<Filme> filmes { get; set; }
+
+        public FilmesContext(DbContextOptions<FilmesContext> options) : base(options) { }
+        
+          
+    }
+}
---
+++ Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesCadastro.Models
+{
+    public class Filme
+    {
+        
+        public int id { get; set; }
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        public string nome { get; set; }
+        [Required(ErrorMessage = "A nota é obrigatória")]
+        public double nota { get; set; }
+        [Required(ErrorMessage = "Uma breve descrição é obrigatória")]
+        public string descricao { get; set; }
+    }
+}
